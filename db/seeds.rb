1c9294070a94ca7926a8529d3a9bfbe8b555eaac
grchibi <tomoyukit2@gmail.com>
2019-07-09 01:16:07 +0000

    ポスグレ対応

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-IotDevice.create({ name: 'Big Mother', desc: '1st mother'})
+IotDevice.create({ name: 'raspberrypi-wh-1', desc: '1st zero wh'})
