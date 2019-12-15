41072c07e4a454cf50c260d9a2f55f028416f5b4
grchibi <tomoyukit2@gmail.com>
2019-12-15 06:37:01 +0000

    modified Refer API to process params.

--- app/controllers/api/v1/tph_refer_controller.rb
+++ app/controllers/api/v1/tph_refer_controller.rb
@@ -11,23 +11,41 @@ module Api
                 begin
                     ret_json = { text: 'UNKO' }
                     
-                    #    select tph_a.id, tph_a.dt, tph_a.t, tph_a.p, tph_a.h, tph_a.iot_device_id
-                    #    from tph_records as tph_a
-                    #    inner join (select iot_device_id, MAX(created_at) AS MaxCa from tph_records group by iot_device_id) as tph_b                          
-                    #    on tph_a.iot_device_id = tph_b.iot_device_id
-                    #    and tph_a.created_at = tph_b.MaxCa;
-                    latests = TphRecord.find_by_sql(%|
-                        SELECT tph_a.id, tph_a.dt, tph_a.t, tph_a.p, tph_a.h, tph_a.iot_device_id, iot_devices.uname
-                        FROM tph_records AS tph_a
-                        INNER JOIN (
-                            SELECT iot_device_id, MAX(created_at) AS MaxCa
-                            FROM tph_records
-                            GROUP BY iot_device_id) as tph_b
-                        ON tph_a.iot_device_id = tph_b.iot_device_id and tph_a.created_at = tph_b.MaxCa
-                        LEFT JOIN iot_devices
-                        ON tph_a.iot_device_id = iot_devices.id
-                    |)
-    
+                    latests = ''
+                    if (params.has_key?(:loc) && !params[:loc].blank?)
+                        loc_param = params[:loc]   # device location
+                        logger.info("LOC => #{loc_param}")
+                        
+                        #    select tph.dt, tph.t, tph.p, tph.h, MAX(tph.created_at) AS MaxCa, loc.loc_ident
+                        #    from tph_records tph, device_locations loc
+                        #    where tph.iot_device_id = loc.iot_device_id and loc.loc_ident = 'WORD';
+                        latests = TphRecord.find_by_sql(%|
+                            SELECT tph.dt, tph.t, tph.p, tph.h, MAX(tph.created_at) AS MaxCa, loc.loc_ident
+                            FROM tph_records tph, device_locations loc
+                            WHERE tph.iot_device_id = loc.iot_device_id and loc.loc_ident = '#{loc_param}'
+                        |)
+                        
+                    else
+                        logger.info("LOC UNDEFINED.")
+                        
+                        #    select tph_a.id, tph_a.dt, tph_a.t, tph_a.p, tph_a.h, tph_a.iot_device_id
+                        #    from tph_records as tph_a
+                        #    inner join (select iot_device_id, MAX(created_at) AS MaxCa from tph_records group by iot_device_id) as tph_b                          
+                        #    on tph_a.iot_device_id = tph_b.iot_device_id
+                        #    and tph_a.created_at = tph_b.MaxCa;
+                        latests = TphRecord.find_by_sql(%|
+                            SELECT tph_a.id, tph_a.dt, tph_a.t, tph_a.p, tph_a.h, tph_a.iot_device_id, iot_devices.uname
+                            FROM tph_records AS tph_a
+                            INNER JOIN (
+                                SELECT iot_device_id, MAX(created_at) AS MaxCa
+                                FROM tph_records
+                                GROUP BY iot_device_id) as tph_b
+                            ON tph_a.iot_device_id = tph_b.iot_device_id and tph_a.created_at = tph_b.MaxCa
+                            LEFT JOIN iot_devices
+                            ON tph_a.iot_device_id = iot_devices.id
+                        |)
+                    end
+                    
                     ret_json = { 'result': latests }
                     ret_sts = :ok
                     
