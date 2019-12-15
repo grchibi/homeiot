6e22548466bef5ed313b2d9145965ef88eae1c67
grchibi <tomoyukit2@gmail.com>
2019-12-15 07:33:09 +0000

    modify device_locations table.

--- db/migrate/20191215021249_create_device_locations.rb
+++ db/migrate/20191215021249_create_device_locations.rb
@@ -1,7 +1,7 @@
 class CreateDeviceLocations < ActiveRecord::Migration[5.0]
   def change
     create_table :device_locations do |t|
-      t.string :loc_ident, :null => false
+      t.string :loc_ident, :null => false, index: { unique: true }
       t.string :desc
       t.references :iot_device
       t.timestamps
